0f5318741bbd68f631c79338ed581391738a1c9e
Denis <130508415+shtunzer@users.noreply.github.com>
2023-05-01 20:47:51 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".firstSwiper", {
+    navigation: {
+      nextEl: ".firstSwiper-button-next",
+      prevEl: ".firstSwiper-button-prev",
+    },
+});
+
+var swiper2 = new Swiper(".projectsSwiper", {
+  navigation: {
+    nextEl: ".projectsSwiper-button-next",
+    prevEl: ".projectsSwiper-button-prev",
+  },
+});
+
+var swiper3 = new Swiper(".securitySwiper", {
+  navigation: {
+    nextEl: ".securitySwiper-button-next",
+    prevEl: ".securitySwiper-button-prev",
+  },
+});
